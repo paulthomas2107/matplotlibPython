64e56d3683ac8a81d381c8ac2360120b5936dae4
Paul Thomas <paulthomas2107@gmail.com>
2022-11-04 19:59:08 +0000

    More...

--- main.py
+++ main.py
@@ -3,5 +3,29 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import random
 
+reg = LinearRegression()
 x_values = []
 y_values = []
+
+for i in range(1000):
+
+    plt.clf()
+
+    x_values.append(random.randint(0, 100))
+    y_values.append(random.randint(0, 100))
+
+    x2 = np.array(x_values)
+    x = x2.reshape(-1, 1)
+
+    y2 = np.array(y_values)
+    y = y2.reshape(-1, 1)
+
+    if i % 20 == 0:
+        reg.fit(x, y)
+        plt.xlim(0, 100)
+        plt.ylim(0, 100)
+        plt.scatter(x_values, y_values, color="black")
+        plt.plot(list(range(100)), reg.predict(np.array([x for x in range(100)]).reshape(-1, 1)))
+        plt.pause(0.0001)
+
+plt.show()
